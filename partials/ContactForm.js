7c7f440623054c9f24b3fd0dfcf3ae5146babe2b
Sid <sid.ghodke@gmail.com>
2021-09-14 20:43:02 -0700

    Update ContactForm style

--- partials/ContactForm.js
+++ partials/ContactForm.js
@@ -5,16 +5,15 @@ export default function ContactForm() {
         <p>
             Feel free to contact me. I can be reached by sending a message to <a
             href="#">sid.ghodke@gmail.com</a> or by using the form below.
-            <div>
-                <form className={styles.contactForm} action="MAILTO:sid.ghodke@gmail.com" method="get" encType="text/plain">
-                    <input className={styles.inputBlockLevel} type="text" name="subject" placeholder="Name" required/>
-                    {/*
-                    <input class="input-block-level" type="email" name="email" placeholder="Email" required />
-                    */}
-                    <textarea className={styles.inputBlockLevel} name="body" placeholder="Message" rows="5" required/>
-                    <button type="submit" className={styles.sendButton}>SEND</button>
-                </form>
-            </div>
         </p>
+        <form className={styles.contactForm} action="MAILTO:sid.ghodke@gmail.com" method="get" encType="text/plain">
+            <input className={styles.inputBlockLevel} type="text" name="subject" placeholder="Name" required/>
+            {/*
+            <input class="input-block-level" type="email" name="email" placeholder="Email" required />
+            */}
+            <textarea className={styles.inputBlockLevel} name="body" placeholder="Message" rows="5" required/>
+            <button type="submit" className={styles.sendButton}>SEND</button>
+        </form>
+
     </section>
 }
