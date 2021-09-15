e53b388c1f0ccae7fd3ce5847754605bc8e54bc7
Sid <sid.ghodke@gmail.com>
2021-09-14 20:43:34 -0700

    Add striped project style

--- components/Project.js
+++ components/Project.js
@@ -1,6 +1,8 @@
+import styles from '../styles/Home.module.css'
+
 export default function Project({ children, className }) {
     return (
-        <section className={className}>
+        <section className={styles.article}>
             { children }
         </section>
     )
