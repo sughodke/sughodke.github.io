b248a6e6ae5a9b2cc806877f8dcfff30162fddb5
Sid <sid.ghodke@gmail.com>
2021-09-14 22:07:47 -0700

    Updates

--- _next/static/Bvdeh7-oL7I9gwtiuwVJr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2a17820aa3bd396c40a3.css","static/chunks/pages/index-72d6f1647ca83bed8330.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/nLFVheBq2TrF2_iH1rTqB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2a17820aa3bd396c40a3.css","static/chunks/pages/index-03d161784b53c777a446.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
