0b435759e477390cf591bbf59328ab66e6d03f62
Sid <sid.ghodke@gmail.com>
2021-09-15 22:05:32 -0700

    Updates

---
+++ _next/static/7e_RL-KSRsbvRZDffn5K7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2a17820aa3bd396c40a3.css","static/chunks/pages/index-27ef2e6b83a19ddb0775.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
