cacf6796c25ba89ba35aba76c577bab5f974dc84
Sid <sid.ghodke@gmail.com>
2021-09-15 19:33:17 -0700

    Update bio headline
    Cleanup code

--- partials/Header.js
+++ partials/Header.js
@@ -5,17 +5,15 @@ export default function Header() {
     return <>
     <header className={styles.header}>
         <h1 className={styles.title}>Sid Ghodke</h1>
-        {/*
-        <Image src="/avatar.jpeg" alt="Sid Ghodke avatar" width={224} height={224}/>
-        */}
+        {/* <Image src="/avatar.jpeg" alt="Sid Ghodke avatar" width={224} height={224}/> */}
         <img src="/avatar.jpeg" alt="Sid Ghodke avatar"/>
     </header>
 
     <section className={styles.about}>
         <p>
-            I'm Sid, I live in Northern Califonia. These days I'm involved mostly in software - but I still
-            find time for surfing, reading, writing, and beer. A lot of my recent work involves
-            visualization and machine learning. I blog at <a href="//medium.com/@sid.ghodke">on Medium</a>.
+            I&apos;m Sid, I live in Northern California. These days I&apos;m involved mostly in software - but I still
+            find time for surfing, reading, and writing. A lot of my recent work involves
+            digital health and team building. I blog at <a href="//medium.com/@sid.ghodke">on Medium</a>.{' '}
             <Link href="/Siddharth%20Ghodke%20(Digital%20Health%20Leader).pdf">My Resume</Link>.
         </p>
     </section>
