505dc54124e4979ddf9aaaa788542d12a85c053c
Sid <sid.ghodke@gmail.com>
2022-02-12 15:14:37 -0800

    Update my headline

--- partials/Header.js
+++ partials/Header.js
@@ -9,13 +9,12 @@ export default function Header() {
         <img src="/avatar.jpeg" alt="Sid Ghodke avatar"/>
     </header>
 
-    <section className={styles.about}>
-        <p>
-            I&apos;m Sid, I live in Northern California. These days I&apos;m involved mostly in software - but I still
-            find time for surfing, reading, and writing. A lot of my recent work involves
-            digital health and team building. I blog at <a href="//medium.com/@sid.ghodke">on Medium</a>.{' '}
-            <Link href="/Siddharth%20Ghodke%20(Digital%20Health%20Leader).pdf">My Resume</Link>.
-        </p>
-    </section>
+    <section className={styles.about}><p>
+        I&apos;m Sid, I live in Northern California.
+        I am an entrepreneur engineer working in the intersection of Healthcare, Technology and Capital.
+        When I can I make time for surfing, reading, and writing.
+        I blog <a href="//medium.com/@sid.ghodke">on Medium</a>.{' '}
+        And this is <Link href="/Siddharth%20Ghodke%20(Digital%20Health%20Leader).pdf">my Resume</Link>.
+    </p></section>
     </>
 }
