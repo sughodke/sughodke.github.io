8d1defeb24421facdb9e481d8d408033d2419011
Sid <sid.ghodke@gmail.com>
2022-02-12 15:10:53 -0800

    Remove Google Plus from Social Links
    Shrink padding

--- partials/SocialLinks.js
+++ partials/SocialLinks.js
@@ -6,7 +6,7 @@ export default function SocialLinks() {
             <a href="//stackoverflow.com/users/721564/sid-ghodke"><i className="fab fa-stack-overflow"/></a>
             <a href="//facebook.com/sid.ghodke"><i className="fab fa-facebook"/></a>
             <a href="//twitter.com/MultigrainSid"><i className="fab fa-twitter"/></a>
-            <a href="//plus.google.com/+SidGhodke/posts"><i className="fab fa-google-plus-g"/></a>
+            <a href="//google.com/search?q=Sid%20Ghodke"><i className="fab fa-google"/></a>
             <a href="//linkedin.com/in/sidghodke"><i className="fab fa-linkedin"/></a>
             <a href="//github.com/sughodke"><i className="fab fa-github-alt"/></a>
             <a href="//bitbucket.com/sid_ghodke"><i className="fab fa-bitbucket"/></a>
