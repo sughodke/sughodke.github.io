85052aa16676851e771b64c188f3d7b0920d873b
Sid <sid.ghodke@gmail.com>
2022-02-12 15:12:03 -0800

    Simplify the contact us CTA Add email obfuscation

--- partials/ContactForm.js
+++ partials/ContactForm.js
@@ -3,17 +3,9 @@ import styles from "../styles/ContactForm.module.css";
 export default function ContactForm() {
     return <section className={styles.contact}>
         <p>
-            Feel free to contact me. I can be reached by sending a message to <a
-            href="#">sid.ghodke@gmail.com</a> or by using the form below.
-        </p>
-        <form className={styles.contactForm} action="MAILTO:sid.ghodke@gmail.com" method="get" encType="text/plain">
-            <input className={styles.inputBlockLevel} type="text" name="subject" placeholder="Name" required/>
-            {/*
-            <input class="input-block-level" type="email" name="email" placeholder="Email" required />
-            */}
-            <textarea className={styles.inputBlockLevel} name="body" placeholder="Message" rows="5" required/>
-            <button type="submit" className={styles.sendButton}>SEND</button>
-        </form>
+            Drop me a line if you are seeking investment or advice.
 
+            <a href="MAILTO:sid.ghodke@gmail.com">sid (dot) ghodke@gmail.com</a>
+        </p>
     </section>
 }
