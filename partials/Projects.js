34de9da54016c21b37a3952d8509c372be09d4e1
Sid <sid.ghodke@gmail.com>
2022-02-12 15:15:53 -0800

    Stream-line Projects content to talk about what I actually did
    Remove dead-links and fake stuff
    Pull out Projects from index page

---
+++ partials/Projects.js
@@ -0,0 +1,42 @@
+const Projects = [
+  <p>
+    I’ve been funding and building Digital Health software for the past ten years.
+    Here&apos;s some of the highlights:
+    <ul>
+      <li>grew a $150M digital coaching platform for type-2 diabetes, smoking cessation
+        and heart health programs.</li>
+      <li>investor in 4 health tech start ups</li>
+      <li>leading engineering teams (50+ engineers) to build kick-butt care</li>
+      <li>founded a company, from vision to funding to paying customers to acquisition</li>
+    </ul>
+  </p>,
+]
+  /*
+  <p><a>Advising Startups</a><br/>
+    Building software to improve patient care is where my professional passion
+    lies. I thrive when great engineering, solid execution and focused care
+    supports everyone. I love creating systems and teams that accomplish great things while
+    nurturing talent, providing opportunities and helping all involved grow.
+  </p>,
+
+  <p><a>Investing</a><br/>
+    Real Estate
+    Startups
+    SaaS companies
+    Whether it’s the patient, the care provider or the
+    engineers,
+  </p>,
+
+  <p><a>Speaking Engagements</a><br/>
+  </p>,
+
+  <p>Two years ago I started <a href="http://172.93.55.89">AlohaMD</a>, a telehealth consulting
+    service in the SF Bay Area. We&apos;ve stopped taking on new contracts. See our case-studies for the
+    work we undertook.</p>,
+
+  <p>I&apos;m also building <a href="#">Sanjay the IT Guy</a>, a time management game (to the likes of
+    DinerDash) that puts you in the shoes of Sanjay an under-paid IT Guy who has to put out literal
+    and figurative fires in his office.</p>,
+] */
+
+export default Projects
