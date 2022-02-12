7c361b059dc5ccf26a34adde46ff9e5e9cea1f10
Sid <sid.ghodke@gmail.com>
2022-02-12 15:23:10 -0800

    Updates

---
+++ _next/static/9TUIQ_b8p1Mzpawju-JGW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/01143b97038053fe18f3.css","static/chunks/pages/index-735e9aedb5b742329a61.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
